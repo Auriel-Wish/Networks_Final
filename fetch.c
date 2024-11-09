23149edca11d7dd5ab8a07fc4496cf8cbca77669
LiamDrew <liamdrew92@icloud.com>
2024-11-09 12:30:47 -0500

    Scaffolding for proxy server and SSL requests

---
+++ fetch.c
@@ -0,0 +1,90 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <openssl/ssl.h>
+#include <openssl/err.h>
+
+#define HOSTNAME "google.com"
+#define PORT "443"
+#define REQUEST "GET / HTTP/1.1\r\nHost: " HOSTNAME "\r\nConnection: close\r\n\r\n"
+
+// int main() {
+//     SSL_library_init();                   // Initialize the OpenSSL library
+//     SSL_load_error_strings();              // Load error strings for diagnostics
+//     OpenSSL_add_ssl_algorithms();          // Load SSL algorithms
+
+//     const SSL_METHOD *method = TLS_client_method();   // Set the TLS method to use
+//     SSL_CTX *ctx = SSL_CTX_new(method);    // Create an SSL context
+//     if (ctx == NULL) {
+//         ERR_print_errors_fp(stderr);
+//         return 1;
+//     }
+
+//     // Create a socket and connect to the host
+//     struct addrinfo hints, *res;
+//     memset(&hints, 0, sizeof(hints));
+//     hints.ai_family = AF_INET;
+//     hints.ai_socktype = SOCK_STREAM;
+
+//     if (getaddrinfo(HOSTNAME, PORT, &hints, &res) != 0) {
+//         perror("getaddrinfo");
+//         SSL_CTX_free(ctx);
+//         return 1;
+//     }
+
+//     int sock = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
+//     if (sock < 0) {
+//         perror("socket");
+//         freeaddrinfo(res);
+//         SSL_CTX_free(ctx);
+//         return 1;
+//     }
+
+//     if (connect(sock, res->ai_addr, res->ai_addrlen) != 0) {
+//         perror("connect");
+//         close(sock);
+//         freeaddrinfo(res);
+//         SSL_CTX_free(ctx);
+//         return 1;
+//     }
+//     freeaddrinfo(res);
+
+//     // Create an SSL object and attach it to the socket
+//     SSL *ssl = SSL_new(ctx);
+//     SSL_set_fd(ssl, sock);
+//     if (SSL_connect(ssl) <= 0) {          // Perform the SSL/TLS handshake
+//         ERR_print_errors_fp(stderr);
+//         SSL_free(ssl);
+//         close(sock);
+//         SSL_CTX_free(ctx);
+//         return 1;
+//     }
+
+//     // Send the HTTP GET request
+//     if (SSL_write(ssl, REQUEST, strlen(REQUEST)) <= 0) {
+//         ERR_print_errors_fp(stderr);
+//         SSL_free(ssl);
+//         close(sock);
+//         SSL_CTX_free(ctx);
+//         return 1;
+//     }
+
+//     // Receive and print the response
+//     char buffer[4096];
+//     int bytes;
+//     while ((bytes = SSL_read(ssl, buffer, sizeof(buffer) - 1)) > 0) {
+//         buffer[bytes] = '\0';
+//         printf("%s", buffer);
+//     }
+
+//     // Clean up
+//     SSL_free(ssl);
+//     close(sock);
+//     SSL_CTX_free(ctx);
+//     EVP_cleanup();
+
+//     return 0;
+// }
--- proxy.c
+++ proxy.c
@@ -1,90 +1,149 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include <unistd.h>
+#include <string.h>
+#include <stdbool.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
 #include <arpa/inet.h>
 #include <netdb.h>
-#include <openssl/ssl.h>
-#include <openssl/err.h>
 
-#define HOSTNAME "google.com"
-#define PORT "443"
-#define REQUEST "GET / HTTP/1.1\r\nHost: " HOSTNAME "\r\nConnection: close\r\n\r\n"
+#define BUF_TTL 3
 
-int main() {
-    SSL_library_init();                   // Initialize the OpenSSL library
-    SSL_load_error_strings();              // Load error strings for diagnostics
-    OpenSSL_add_ssl_algorithms();          // Load SSL algorithms
+int main(int argc, char **argv)
+{
+    int portno;
+    int parentfd;
 
-    const SSL_METHOD *method = TLS_client_method();   // Set the TLS method to use
-    SSL_CTX *ctx = SSL_CTX_new(method);    // Create an SSL context
-    if (ctx == NULL) {
-        ERR_print_errors_fp(stderr);
-        return 1;
+    struct sockaddr_in serveraddr;
+    struct hostent *hostp;
+    char *hostaddrp;
+
+    fd_set active_fd_set, read_fd_set;
+
+    if (argc != 2) {
+        fprintf(stderr, "Usage: %s <port>\n", argv[0]);
+        exit(EXIT_FAILURE);
     }
 
-    // Create a socket and connect to the host
-    struct addrinfo hints, *res;
-    memset(&hints, 0, sizeof(hints));
-    hints.ai_family = AF_INET;
-    hints.ai_socktype = SOCK_STREAM;
+    portno = atoi(argv[1]);
+    printf("Listening on port %d\n", portno);
 
-    if (getaddrinfo(HOSTNAME, PORT, &hints, &res) != 0) {
-        perror("getaddrinfo");
-        SSL_CTX_free(ctx);
-        return 1;
+    parentfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (parentfd < 0) {
+        perror("Error opening socket");
+        exit(EXIT_FAILURE);
     }
 
-    int sock = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
-    if (sock < 0) {
-        perror("socket");
-        freeaddrinfo(res);
-        SSL_CTX_free(ctx);
-        return 1;
+    int optval = 1;
+    setsockopt(parentfd, SOL_SOCKET, SO_REUSEADDR,
+               (const void *)&optval, sizeof(int));
+
+    // build the server's internet address
+    bzero((char *)&serveraddr, sizeof(serveraddr));
+    serveraddr.sin_family = AF_INET;
+    serveraddr.sin_addr.s_addr = htonl(INADDR_ANY);
+    serveraddr.sin_port = htons((unsigned short)portno);
+
+    // bind the parent socket to the input portno
+    if (bind(parentfd, (struct sockaddr *)&serveraddr, sizeof(serveraddr)) < 0)
+        perror("Error on binding");
+
+    if (listen(parentfd, 0) < 0)
+        perror("Error on listen");
+
+
+    /* Initialize the set of active sockets */
+    FD_ZERO (&active_fd_set);
+    FD_SET (parentfd, &active_fd_set);
+
+    struct sockaddr_in clientaddr;
+
+    // Dispatch_T *dispatch = new_dispatch();
+
+    struct timeval timeout;
+    timeout.tv_sec = BUF_TTL;    //setting the default timeout
+    timeout.tv_usec = 0;
+
+    while (true) {
+        /* Try to free any buffers older than the TTL
+         * If a buffer is freed, the client is removed from the list and their
+         * connection is closed */
+        // free_old_buffers(dispatch->table, &active_fd_set, dispatch->clients);
+
+        /* For the remaining buffered content, return the time until the next
+         * one will expire */
+        // timeout = get_next_timeout(dispatch->table);
+        printf("rerunning select in %ld seconds\n", timeout.tv_sec);
+
+        read_fd_set = active_fd_set;
+
+        /* SELECT will timeout when the next buffered message expires */
+        if (select (FD_SETSIZE, &read_fd_set, NULL, NULL, &timeout) < 0) {
+            perror("ERROR with select");
+        }
+
+        /* Service all sockets with input pending */
+        for (int i = 0; i < FD_SETSIZE; ++i) {
+            if (FD_ISSET (i, &read_fd_set)) {
+                if (i == parentfd) {
+                    /* Connection request on parent socket */
+                    int new_fd;
+                    socklen_t size = sizeof(clientaddr);
+                    new_fd = accept(parentfd, (struct sockaddr *)&clientaddr, 
+                        &size);
+
+                    if (new_fd < 0) {
+                        perror("Error accepting new socket");
+                    }
+
+                    /* gethostbyaddr: determine who sent the message */
+                    hostp = gethostbyaddr(
+                        (const char *)&clientaddr.sin_addr.s_addr,
+                        sizeof(clientaddr.sin_addr.s_addr), 
+                        AF_INET
+                    );
+
+                    if (hostp == NULL) {
+                        perror("ERROR on gethostbyaddr");
+                    }
+
+                    hostaddrp = inet_ntoa(clientaddr.sin_addr);
+                    if (hostaddrp == NULL) {
+                        perror("Error on inet_ntoa");
+                    }
+
+                    printf("Server established connection with %s (%s)\n\n",
+                        hostp->h_name, hostaddrp);
+
+                    // Adding the new connection request to the set of active
+                    // sockets
+                    FD_SET(new_fd, &active_fd_set);
+
+                    /* The client socket has connected with the current socket, 
+                     * but the client has not identified itself yet. Therefore, 
+                     * as of right now, it's not in the list of active clients
+                     * even though it's in the set of FDs */
+                }
+
+                else {
+                    /* Incoming data from already-connected socket */
+                    // int n = read_message(i, dispatch);
+                    int n = 0;
+
+                    /* Only close the socket if we reach EOF (the client
+                     * closes the connection) */
+                    if (n < 0) {
+                        close(i);
+                        FD_CLR(i, &active_fd_set);
+                    }
+                }
+            }
+        }
     }
 
-    if (connect(sock, res->ai_addr, res->ai_addrlen) != 0) {
-        perror("connect");
-        close(sock);
-        freeaddrinfo(res);
-        SSL_CTX_free(ctx);
-        return 1;
-    }
-    freeaddrinfo(res);
-
-    // Create an SSL object and attach it to the socket
-    SSL *ssl = SSL_new(ctx);
-    SSL_set_fd(ssl, sock);
-    if (SSL_connect(ssl) <= 0) {          // Perform the SSL/TLS handshake
-        ERR_print_errors_fp(stderr);
-        SSL_free(ssl);
-        close(sock);
-        SSL_CTX_free(ctx);
-        return 1;
-    }
-
-    // Send the HTTP GET request
-    if (SSL_write(ssl, REQUEST, strlen(REQUEST)) <= 0) {
-        ERR_print_errors_fp(stderr);
-        SSL_free(ssl);
-        close(sock);
-        SSL_CTX_free(ctx);
-        return 1;
-    }
-
-    // Receive and print the response
-    char buffer[4096];
-    int bytes;
-    while ((bytes = SSL_read(ssl, buffer, sizeof(buffer) - 1)) > 0) {
-        buffer[bytes] = '\0';
-        printf("%s", buffer);
-    }
-
-    // Clean up
-    SSL_free(ssl);
-    close(sock);
-    SSL_CTX_free(ctx);
-    EVP_cleanup();
+    // free_dispatch(&dispatch);
 
     return 0;
 }
